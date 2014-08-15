f9c3ffefed2e093cf157351f1edbc4b1f2b90744
javier <js@silveriofunk.com>
2014-08-15 15:44:31 +0100

    making the broken? method work properly

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -6,4 +6,12 @@ it "should not be broken after we create it" do
 	the_bike = Bike.new
   expect(the_bike).not_to be_broken 
   end
+
+  it "should be able to break" do
+  	bike = Bike.new
+  	bike.break!
+  	expect(bike).to be_broken
+  end
+
 end
+

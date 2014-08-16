62e9688a92969b256e15af041b788c6032425b2b
javier <js@silveriofunk.com>
2014-08-16 21:26:55 +0100

    updated a variable so they all matched

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,8 +3,8 @@ require 'bike'
 describe Bike do
 
 it "should not be broken after we create it" do
-	the_bike = Bike.new
-  expect(the_bike).not_to be_broken 
+	  bike = Bike.new
+    expect(bike).not_to be_broken 
   end
 
   it "should be able to break" do

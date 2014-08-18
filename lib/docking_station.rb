29a98b06f008dff9154e5defc30583a0cb73ec73
javier <js@silveriofunk.com>
2014-08-18 09:30:19 +0100

    updated the docking_station file

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,8 @@
 class DockingStation
 	DEFAULT_CAPACITY = 10
 
-	def initialize
+	def initialize(options = {})
+		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
 		@bikes = []
 	end
 
@@ -15,11 +16,6 @@ class DockingStation
 
 	def release(bike)
 		@bikes.delete(bike)
-	end
-
-	def initialize(options = {})
-		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
-		@bikes = []
 	end
 
 	def full?()
